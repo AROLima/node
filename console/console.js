c9245254c946d820938e075113b1c69cbe774fc4
AroLima <rodrigo@Kyrian>
2024-01-28 22:06:26 -0300

    events2

--- console/console.js
+++ console/console.js
@@ -41,8 +41,4 @@ for (let index = 0; index <= 10; index ++) {
     console.log(index)
     
 }
-
 console.timeEnd('contador')
-
-
-
--- events/events.js
+++ events/events.js
@@ -1,10 +1,18 @@
-const EventEmitter = require('events')
+const {EventEmitter} = require('events')
 
 class Evento extends EventEmitter{}
 const meuEvento = new Evento()
 
+//subscriber - assinante
 meuEvento.on('segurança',(x,y)=>{
     console.log(`Executando o evento 'segurança': ${x} ${y}`)
 })
 
+//emissor - publisher
 meuEvento.emit('segurança', 'UserAdmin', 'Alterou salário')
+
+meuEvento.on('encerrar',(dados)=>{
+    console.log('\nAssinante: ' + dados)
+})
+
+meuEvento.emit('encerrar','Encerrando a execução da importação dos dados')
